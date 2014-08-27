82f0537a6791be5f4a91b961ad39657508b803e1
knarf2011 <dugganfrank@comcast.net>
2014-08-27 09:31:25 -0400

    fixed warning in CommandExecutorHelp.java

--- src/main/java/knarf2011/becquerel/commands/CommandExecutorHelp.java
+++ src/main/java/knarf2011/becquerel/commands/CommandExecutorHelp.java
@@ -10,7 +10,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
 
 public class CommandExecutorHelp implements CommandExecutor
 {
